a9cf92b9bbeefcefe050df0f35926a2a6aad642e
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-10-19 13:21:14 +0530

    admin added

--- src/pages/customer/book/Book.jsx
+++ src/pages/customer/book/Book.jsx
@@ -1,48 +1,119 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/authProvider";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 
 const Book = () => {
-    const [services, setServices] = useState([])
-    const { user } = useContext(AuthContext)
-    // console.log(user);
-    fetch('http://localhost:5000/services')
+    const [services, setServices] = useState([]);
+    const [selectedService, setSelectedService] = useState(null);
+    const { user } = useContext(AuthContext);
+    const { register, handleSubmit, setValue, reset,errors } = useForm();
 
-        .then(res => res.json())
-        .then(data => {
-            setServices(data)
+    useEffect(() => {
+        fetch("http://localhost:5000/services")
+            .then((res) => res.json())
+            .then((data) => {
+                setServices(data);
+            });
+    }, []);
+
+    const onSubmit = (data) => {
+        console.log(data);
+        fetch('http://localhost:5000/bookings', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
         })
+            .then(res => res.json())
+            .then(data => {
+                reset()
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your booking is processing !!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+    };
+
     return (
-        <div className="bg-orange-50  w-full pt-12 ps-10 pb-40">
+        <div className="bg-orange-50 w-full pt-12 ps-10 pb-40">
             <nav className="flex justify-around">
                 <h2 className="font-poppins font-medium text-2xl text-[#0C0C0C]">Book</h2>
-                <h2 className="font-poppins font-medium text-2xl text-[#0C0C0C]">{user?.displayName}</h2>
+                <h2 className="font-poppins font-medium text-2xl text-[#0C0C0C]">
+                    {user?.displayName}
+                </h2>
             </nav>
-            <div className="hero-content  text-center">
-                <div className="card flex-shrink-0 w-full max-w-md border">
-                    <div className="card-body ">
+            <div className="hero-content text-center">
+                <form
+                    className="card flex-shrink-0 w-full max-w-md border"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <div className="card-body">
                         <div className="form-control">
-                            <input type="text" defaultValue={user?.displayName} className="input input-bordered font-Montserrat font-medium text-base text-black" />
+                            <input
+                                type="text"
+                                {...register("displayName", { required: true })}
+                                value={user?.displayName}
+                                onChange={(e) => setValue("displayName", e.target.value)}
+                                className="input input-bordered font-Montserrat font-medium text-base text-black"
+                            />
                         </div>
                         <div className="form-control">
-                            <input type="text" defaultValue={user?.email} className="input input-bordered font-Montserrat font-medium text-base text-black" />
+                            <input
+                                type="text"
+                                {...register("email", { required: true })}
+                                value={user?.email}
+                                onChange={(e) => setValue("email", e.target.value)}
+                                className="input input-bordered font-Montserrat font-medium text-base text-black"
+                            />
                         </div>
                         <div className="form-control">
-                        <select name="" id="" className="rounded-2xl p-4">
-                            {
-                                services.map(item=> <option key={item._id} value={item.serviceName}><p className="input input-bordered font-Montserrat font-medium text-base text-black">{item?.serviceName}</p></option>)
-                            } 
-                                </select>
+                            <select
+                                name="serviceName"
+                                {...register("serviceName", { required: true })}
+                             
+                                className="rounded-2xl p-4"
+                                onChange={(e) => {
+                                    const selectedServiceName = e.target.value;
+                                    const selectedService = services.find((service) => service.serviceName === selectedServiceName);
+                                    setSelectedService(selectedService?.price || 0);
+                                }}
+                            >
+                                {services.map((item) =>
+                                    <option key={item._id}>
+                                        {item?.serviceName}
+        
+                                    </option>
 
+                                )
+                                }
+                            </select>
                         </div>
                         <div className="pay-wrapper flex justify-between items-center">
-                            <h3 className="font-Montserrat font-medium text-black text-base">Your service charge will be :</h3>
-                            <button type="submit" className='font-poppins font-semibold text-sm py-2 px-10 bg-[#F63E7B] rounded-md text-[#FFF] mt-6 mb-4'>Pay</button>
+                            <h3 className="font-Montserrat font-medium text-black text-base">
+                                Your service charge will be: {selectedService}
+                            </h3>
+                            
+                         { selectedService != 0 &&  <button
+                                type="submit"
+                                className="font-poppins font-semibold text-sm py-2 px-10 bg-[#F63E7B] rounded-md text-[#FFF] mt-6 mb-4"
+                            >
+                                pay
+                            </button>
+                            }
+                            
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
-
         </div>
     );
 };
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,11 @@
-import { AiOutlineLock, AiOutlineMessage, AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineLock, AiOutlineMessage, AiOutlineShoppingCart ,AiOutlinePlus} from "react-icons/ai";
+import { BsFillPersonPlusFill} from "react-icons/bs";
+import { MdOutlineManageSearch} from "react-icons/md";
 import { NavLink, Outlet } from "react-router-dom";
 import logo from '../../assets/icons/logo.png'
 
 const Dashboard = () => {
+    const admin =false;
     return (
         <div>
             <div className="drawer lg:drawer-open">
@@ -11,7 +14,7 @@ const Dashboard = () => {
                     {<Outlet></Outlet>}
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
                 </div>
-                <div className="drawer-side bg-yellow-300">
+             {admin &&    <div className="drawer-side bg-yellow-300">
                     <img className="w-28 mt-10 ms-6" src={logo} alt="" />
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 min-h-full  text-base-content">
@@ -28,6 +31,33 @@ const Dashboard = () => {
                         <li className="font-poppins font-medium text-base text-[#878787]">
                             <NavLink to="/dashboard/review" activeClassName="active-link">
                                 <AiOutlineMessage /> Review
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                }
+                   <div className="drawer-side bg-yellow-300">
+                    <img className="w-28 mt-10 ms-6" src={logo} alt="" />
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 min-h-full  text-base-content">
+                        <li className="font-poppins font-medium text-base text-[#878787]">
+                            <NavLink to="/dashboard/book" activeClassName="active-link">
+                                <AiOutlineShoppingCart /> Order List
+                            </NavLink>
+                        </li>
+                        <li className="font-poppins font-medium text-base text-[#878787]">
+                            <NavLink to="/dashboard/booking-list" activeClassName="active-link">
+                                <AiOutlinePlus /> Add Service
+                            </NavLink>
+                        </li>
+                        <li className="font-poppins font-medium text-base text-[#878787]">
+                            <NavLink to="/dashboard/review" activeClassName="active-link">
+                                <BsFillPersonPlusFill /> Make Admin
+                            </NavLink>
+                        </li>
+                        <li className="font-poppins font-medium text-base text-[#878787]">
+                            <NavLink to="/dashboard/review" activeClassName="active-link">
+                                <MdOutlineManageSearch /> Manage Service
                             </NavLink>
                         </li>
                     </ul>
